da9939e494abf1b075ba35fe569f1c38e0e447d0
Charlie <porthose.cjsmo@gmail.com>
2021-01-03 05:17:27 -0800

    added sqlstuff

--- secCamLib.py
+++ secCamLib.py
@@ -11,4 +11,15 @@ with open(y) as f:
 
 dbname = dbfactory.DbFactory().create()
 con = sqlite3.connect(dbname)
-cur = con.cursor()
+
+class SqlStuff:
+    def get_ten_records(self):
+        cur = con.cursor()
+        cur.execute("SELECT Date Time FROM SecCams")
+        zoo = cur.fetchall()
+        for z in zoo:
+            print(z)
+
+if __name__ == '__main__' :
+    ss = SqlStuff()
+    ss.get_ten_records()
