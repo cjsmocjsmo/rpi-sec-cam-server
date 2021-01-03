dad6533bcc783759fee7ed0f6e24f9ca9a6f4400
Charlie <porthose.cjsmo@gmail.com>
2021-01-02 18:50:36 -0800

    fixed function

--- secCam-server.py
+++ secCam-server.py
@@ -245,7 +245,7 @@ class last_health_eventHandler(tornado.web.RequestHandler):
         p.parse_logs()
         lhe = p.last_health_event()
         result = ""
-        if len(lhe != 0):
+        if len(lhe) != 0:
             result = lhe
         else:
             result = ["None noted"]
