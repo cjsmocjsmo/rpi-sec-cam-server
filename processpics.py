9709ad83effaa17400ac252bea016fb5e87acef3
Charlie <porthose.cjsmo@gmail.com>
2021-01-07 23:58:18 -0800

    update

--- processpics.py
+++ processpics.py
@@ -22,48 +22,50 @@ with open(y) as f:
 dbname = dbfactory.DbFactory().create()
 con = sqlite3.connect(dbname)
 cur = con.cursor()
-cur.execute('''CREATE TABLE IF NOT EXISTS SecCams (Dir text,
-            Ext text, Prefix text, Name text, Camera text, Date integer,
-            Time text, B64Image text)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS SecCams (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Dir TEXT,
+    Ext TEXT,
+    Prefix TEXT,
+    Name TEXT,
+    Camera TEXT,
+    Date integer,
+    Time TEXT,
+    Picture BLOB);''')
 cur.execute('''CREATE UNIQUE INDEX IF NOT EXISTS TimeIndex ON SecCams (Time)''')
 client = None
 
 class ProcessSecCamPics:
-    def __init__(self):
+    # def __init__(self):
 
-        self.picdir = conf["image_dir"]
-        self.dbdir = conf["db_dir"]
+        # self.picdir = conf["image_dir"]
+        # self.dbdir = conf["db_dir"]
 
-    def get_dir_names(self):
-        pic_dir_glob_path = self.picdir + "/*"
-        return glob.glob(pic_dir_glob_path)
+    # def get_dir_names(self):
+    #     pic_dir_glob_path = self.picdir + "/*"
+    #     return glob.glob(pic_dir_glob_path)
 
 
-    def create_b64_image(self, location):
-        try:	
-            with open(location, 'rb') as imagefile:
-                return ''.join(('data:image/png;base64,', base64.b64encode(imagefile.read()).decode('utf-8')))
-        except FileNotFoundError:
-            print("get_b64_image error")
-            print(location)
-            return "None"
+    # def create_b64_image(self, location):
+    #     try:	
+    #         with open(location, 'rb') as imagefile:
+    #             return ''.join(('data:image/png;base64,', base64.b64encode(imagefile.read()).decode('utf-8')))
+    #     except FileNotFoundError:
+    #         print("get_b64_image error")
+    #         print(location)
+    #         return "None"
 
-    def chop_name(self, p, b64image):
-        dir, ext = os.path.splitext(p)
+    def chop_name(self, fname, data):
+        dir, ext = os.path.splitext(fname)
         prefix, name = os.path.split(dir)
         rawDate, rawTime = name.split("T", 1)
         camera, dAte = rawDate.split("-", 1)
         timE = str(rawTime)
-        # hr, min, sec, ms = rawTime.split(".", 3)
-        # x  = (
-        #     dirt, ext, prefix, name, camera,
-        #     dAte, hr, min, sec, ms, b64image,
-        # )
         return (
-            dir, ext, prefix, name, camera, dAte, timE, b64image,
+            dir, ext, prefix, name, camera, dAte, timE, sqlite3.Binary(data),
         )
 
-    def send_to_dropbox(self, pic):
+    def send_to_dropbox(self, picname, picdata):
         
         if conf["use_dropbox"]:
             #connect to dropbox and start the session authorization process
@@ -73,8 +75,8 @@ class ProcessSecCamPics:
             timestamp = datetime.datetime.now()
             ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
             t = TempImage()
-            with open(pic, "rb") as f:
-                f.write(t.path, pic)
+            with open(picname, "rb") as f:
+                f.write(t.path, picdata)
 
  
             
@@ -83,30 +85,29 @@ class ProcessSecCamPics:
             client.files_upload(open(t.path, "rb").read(), path)
 
 
-    def main(self):
-        
-
-
+    def main(self, fname, data):
 
         while True:
-            dnames = self.get_dir_names()
-            for dd in dnames:
-                print(dd)
-                newname = dd + "/*jpg"
-                picglob = glob.glob(newname)
-                y = []
-                for p in picglob:
-                    self.send_to_dropbox(p)
-                    b64image = self.create_b64_image(p)
-                    y.append(self.chop_name(p, b64image))
-                    # os.remove(p)
-                cur.executemany('''INSERT INTO SecCams VALUES (?,?,?,?,?,?,?,?)''', y)
-                con.commit()
-                shutil.rmtree(dd)
+            # dnames = self.get_dir_names()
+            # for dd in dnames:
+            #     print(dd)
+            #     newname = dd + "/*jpg"
+            #     picglob = glob.glob(newname)
+            y = []
+            #     for p in picglob:
+            self.send_to_dropbox(fname, data)
+
+
+            # b64image = self.create_b64_image(p)
+            y.append(self.chop_name(fname, data))
+            # os.remove(p)
+            cur.executemany('''INSERT INTO SecCams VALUES (?,?,?,?,?,?,?,?)''', y)
+            con.commit()
+                # shutil.rmtree(dd)
             # print("Db insertion complete")
-            time.sleep(3600) # runs initially then sleeps for an hour
+            # time.sleep(3600) # runs initially then sleeps for an hour
 
 
-if __name__ == '__main__' :
-    PSCP = ProcessSecCamPics()
-    PSCP.main()
+# if __name__ == '__main__' :
+#     PSCP = ProcessSecCamPics()
+#     PSCP.main()
