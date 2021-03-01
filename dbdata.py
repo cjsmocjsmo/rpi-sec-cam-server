2310459310e62d900ea7f3e83e60b4d9d744d151
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-02-28 19:58:54 -0800

    update

--- dbdata.py
+++ dbdata.py
@@ -117,8 +117,10 @@ class Pc1Sql:
             print("this is event{}".format(event))
             tmp_file_name = ".".join((uuid.uuid4().hex, "jpg"))
             tmp_full_path = "/".join((self.tmp_dir, tmp_file_name))
-            tmp_full_path = io.StringIO(event[12])
-            new_pic_list.append(list(tmp_full_path))
+            with open(tmp_full_path, "wb") as outfile:
+                outfile.write(event[12])
+            # tmp_full_path = io.StringIO(event[12])
+            new_pic_list.append(tmp_full_path)
         #cur.close()
         return new_pic_list
 
